3696183d724be8bd2894e3de2c52c6668ea4cc97
Dima <115027192+DimaIakovlev@users.noreply.github.com>
2022-11-20 17:17:53 +0500

    Add files via upload

---
+++ Homework_6/Task_041/Program.cs
@@ -0,0 +1,32 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.Clear();
+Console.Write("Введите числа через пробел: ");
+string elements = Console.ReadLine();
+int[] baseArray = GetArrayFromString(elements);
+
+Console.Write($"Для чисел [{String.Join(", ", baseArray)}] количество положительных чисел: ");
+Console.WriteLine(GetPositiveNum(baseArray));
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+int GetPositiveNum(int[] array)
+{
+    int res = 0;
+    foreach (int item in array)
+    {
+        if (item > 0) res++;
+    }
+    return res;
+}
---
+++ Homework_6/Task_043/Program.cs
@@ -0,0 +1,35 @@
+﻿/*  Напишите программу, которая найдёт точку пересечения двух прямых, 
+    заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+    значения b1, k1, b2 и k2 задаются пользователем.
+    b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) 
+    */
+
+
+Console.Clear();
+Console.Write("\nВведите значения b1 и k1 для первого уровнения через пробел: ");
+string elements1 = Console.ReadLine();
+Console.Write("\nВведите значения b2 и k2 для второго уровнения через пробел: ");
+string elements2 = Console.ReadLine();
+double[] baseArray1 = GetArrayFromString(elements1);
+double[] baseArray2 = GetArrayFromString(elements2);
+
+GetIntersectionPoint(baseArray1, baseArray2);
+
+double[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    double[] res = new double[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = double.Parse(nums[i]);
+    }
+    return res;
+
+}
+void GetIntersectionPoint(double[] array1, double[] array2)
+{
+    double x = (array2[0] - array1[0]) / (array1[1] - array2[1]);
+    double y = (array2[1] * x + array2[0]);
+    Console.Write($"\nКоординаты точки пересечения прямых: [{x},{y}]");
+}
